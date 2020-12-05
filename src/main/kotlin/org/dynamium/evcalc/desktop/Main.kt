24c38bc44f3ca0917e3c118c64f4e2b79221b644
Yegor Yakovenko <ggoraa1029@gmail.com>
2020-12-05 14:28:26 +0200

    use EVCalc theme

--- src/main/kotlin/org/dynamium/evcalc/desktop/Main.kt
+++ src/main/kotlin/org/dynamium/evcalc/desktop/Main.kt
@@ -2,12 +2,12 @@ package org.dynamium.evcalc.desktop
 
 import androidx.compose.desktop.Window
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.MaterialTheme
+import org.dynamium.evcalc.desktop.components.theme.EvcalcTheme
 
 @ExperimentalMaterialApi
 fun main() {
     Window(title = "EVCalc") {
-        MaterialTheme {
+        EvcalcTheme {
             MainScreen()
         }
     }
